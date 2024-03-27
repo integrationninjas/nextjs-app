e00e2eee696ef42edd6c5fec4d0582f597d9a7d3
integrationninjas <integrationninjas@gmail.com>
2024-03-27 22:16:54 +0530

    fix cicd steps

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -9,7 +9,6 @@ const connectMongoDB = async () => {
 
   try {
     const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-    console.log('password' + password);
     const connectionString = `mongodb+srv://integrationninjas:${password}@test.vjuinp4.mongodb.net/?retryWrites=true&w=majority`; // clustore url
     await mongoose.connect(connectionString, {
       useNewUrlParser: true,
